d6408a80a082d1e4fb91a07c4c942b5f6501177b
yp <yp@example.com>
2024-06-20 03:23:08 +0100

    0.05

--- dipress/dipressapp/lib/gen.py
+++ dipress/dipressapp/lib/gen.py
@@ -1,37 +1,41 @@
-import re
 import random
+import re
 import wikipedia
-
+from collections import defaultdict
 
 def get_wikipedia_content(search_query):
     search_results = wikipedia.search(query=search_query, results=1)  # Get the top result
 
     if not search_results or not len(search_results):
         return ''
-    return wikipedia.page(search_results[0])
+    try:
+        return wikipedia.page(search_results[0], auto_suggest=False)
+    except Exception:
+        return ''
 
 
 def generate(search_query):
     page = get_wikipedia_content(search_query)
     if not page or not page.content or len(page.content) == 0:
         return False
-    regex = re.compile("([a-zA-Z0-9']+[.?!,]?)+")
-    results = regex.findall(page.content)
-    num_results = len(results)
-    next_words = {}
-    length = random.randint(80, 120)
-    for i in range(num_results):
-        w = []        
-        for y in range(num_results - 1):
-            if results[y].lower() == results[i].lower():
-                w.append(str(results[y + 1]))
-        next_words[results[i]] = w
-    content = []
-    x = random.choice(list(next_words.keys()))
-    for i in range(length):
-        if len(next_words[x]) == 0:
-            break
-        y = random.choice(next_words[x])
-        content.append(y)
-        x = y
-    return page.url, ' '.join(content)
+
+    mchain = defaultdict(list)    
+    words = re.split('[\n\r\s]+', page.content)
+    length = random.randint(150, 250)
+
+    for i in range(len(words) - 2):
+        k = tuple(words[i:i+2])
+        mchain[k].append(words[i+2])
+    
+    curr = random.choice(list(mchain.keys()))
+    output = list(curr)
+    
+    for i in range(length):  # Limit the length of the generated text
+        try:
+            _word = random.choice(mchain[curr])
+            output.append(_word)
+            curr = tuple([curr[1], _word])
+        except Exception:
+            break  
+
+    return page.url, ' '.join(output)
--- readme.md
+++ readme.md
@@ -1,17 +1,19 @@
 # dipress
 
-This is a very basic Django REST Framework app. User can post a query, and the app will use a relevant Wikipedia article to generate text with [Dissociated press](https://en.wikipedia.org/wiki/Dissociated_press) algorithm, and save it.
+This is a very basic Django REST Framework app. User can post a query, and the app will try to find a relevant Wikipedia article, generate mostly non-sensical text from it with [Dissociated press](https://en.wikipedia.org/wiki/Dissociated_press) algorithm, and save it.
 
 ## API Methods
 
-* **/list** - list IDs and queries of all saved texts
-* **/read/\<id\>** - read saved text by ID
-* **/create** - POST {"query":"user query"} - create and save new text
+* **/list/** - list IDs and queries of all saved texts
+* **/read/\<id\>/** - read saved text by ID
+* **/create/** - POST {"query":"user query"} - create and save new text
 
 ## Demo
 
-* [/list](http://dipress.os32.com:8000/list)
-* [/read](http://dipress.os32.com:8000/read)
-* [/create](http://dipress.os32.com:8000/create)
+* [/list/](https://dipress.os32.com/list/)
+* [/read/](https://dipress.os32.com:8000/read/)
+* [/create/](https://dipress.os32.com/create/)
 
-It's being hosted on a DigitalOcean Droplet, and served with Django devserver (no SSL).
+[The form to POST to /create/](https://dipress.os32.com/)
+
+Dome is being hosted on a DigitalOcean Droplet.
